dfcae95e3f3e8984b0017a08b690447e6c559382
Olumide Bambe <loyalsfc@gmail.com>
2023-01-21 16:17:00 +0100

    added mobile nav and edit instructions

--- src/page/Instructions.js
+++ src/page/Instructions.js
@@ -14,11 +14,11 @@ function Instructions() {
 
                         <p className='my-4 leading-7'>Each question in the quiz is of multiple-choice format. Read each question carefully, and click on  your response that is based on the information covered on the topic in the module.</p>
 
-                        <p className='my-4 leading-7'>After responding to a question, click on the "Next Question" button at the bottom to go to the next questino. After responding to the 8th question, click on "Close" on the top of the window to exit the quiz.</p>
+                        <p className='my-4 leading-7'>After responding to a question, click on the "Next Question" button at the bottom to go to the next questino. After responding to the 10th question, click on "Submit" to go to the next quiz.</p>
 
-                        <p className='my-4 leading-7'>If you select an incorrect response for a question, you can try again until you get the correct response. If you retake the quiz, the questions and their respective responses will be randomized.</p>
+                        <p className='my-4 leading-7'>When you click submit when taking the last quiz, it's going to pop your score on each individual and quiz and as well as your average score </p>
 
-                        <p className='my-4 leading-7'>The total score for the quiz is based on your responses to all questions. If you respond incorrectly to a question or retake a question again and get the correct response, your quiz score will reflect it appropriately. However, your quiz will not be graded, if you skip a question or exit before responding to all the questions.</p>
+                        <p className='my-4 leading-7'>The total score for the quiz is based on your responses to all questions. If you respond incorrectly to a question your quiz score will reflect it appropriately. However, your quiz will not be graded, if you skip a question or exit before responding to all the questions.</p>
                     </article>
                     <Link to='/quiz'><button className='px-6 py-2 bg-green-100 text-sm text-white font-medium'>Start</button></Link>
                 </main>
