01c8b0eafac1646f4c3e06b2cb03b385d689e53f
Olumide Bambe <loyalsfc@gmail.com>
2023-02-05 16:17:20 +0100

    updated readme file, creating settings

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Quiziccal is a quiz platform created with react#
+# Quiziccal
+
+#### About Quizical
+
+Quiziccal is a quiz platform created as a perfect tool for anyone looking to test their knowledge and improve their understanding.
+
+#### Technologoies Used
+
+1. React Js
+2. Tailwind CSS
+3. Firebase authentication
+4. Firebase firestore
+5. React toastify
+
+#### Run code
+
+```sh
+npm start
+```
--- src/page/Settings.js
+++ src/page/Settings.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from '../component/Header'
+import { Context } from '../Context'
 
 function Settings() {
+  const {user} = useContext(Context)
+
   return (
         <div className='container mx-auto'>
             <Header />
             <main>
-                
+                <aside></aside>
+                <section>
+                  <article>
+                    <div>
+                      <img src={user?.photoURL} alt="Existing profile pix" className='w-full' />
+                    </div>
+                  </article>
+                </section>
             </main>
         </div>
   )
